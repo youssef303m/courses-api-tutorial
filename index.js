70c6ff9fdad43ff98d4ba80651b46765a63cae1a
Youssef303M <youssef303m@gmail.com>
2024-01-05 01:20:08 +0200

    Handled 404 requests

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const coursesRouter = require("./routes/courses-routes");
 const mongoose = require("mongoose");
+const httpStatus = require("./utils/httpStatus");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 const URI = process.env.MONGO_URI;
 
@@ -16,6 +17,13 @@ mongoose.connect(URI).then(() => {
 app.use(bodyParser.json());
 
 app.use("/api/courses", coursesRouter);
+
+app.all("*", (req, res) => {
+  return res.status(404).json({
+    status: httpStatus.ERROR,
+    data: { message: "This resource is not available" },
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
