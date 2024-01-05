565eb6c36560f02a008243231b060eed869571f3
Youssef303M <youssef303m@gmail.com>
2024-01-05 05:17:13 +0200

    Password Hashing

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const asyncWrapper = require("../middlewares/asyncWrapper");
 const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
 const httpStatus = require("../utils/httpStatus");
 
 const getAllUsers = asyncWrapper(async (req, res) => {
@@ -11,26 +12,31 @@ const getAllUsers = asyncWrapper(async (req, res) => {
   const skip = (page - 1) * limit;
 
   // Get all users from DB using User model
-  const users = await User.find({}, { __v: false }).limit(limit).skip(skip);
+  const users = await User.find({}, { password: false, __v: false })
+    .limit(limit)
+    .skip(skip);
   return res.json({ status: httpStatus.SUCCESS, data: { users } });
 });
 
 const register = asyncWrapper(async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
+  // Check if user exists
   const userExists = await User.findOne({ email: email });
-
   if (userExists) {
     return res
       .status(400)
       .json({ status: httpStatus.FAIL, message: "User already exists" });
   }
 
+  // Password hashing
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   const newUser = new User({
     firstName,
     lastName,
     email,
-    password,
+    password: hashedPassword,
   });
 
   await newUser.save();
